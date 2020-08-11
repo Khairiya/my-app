8ccaab7cdb046f42a024fc790a2066db12752c7e
unknown <rashagibrill@gmail.com>
2020-08-11 16:32:05 +0000

    Updates

---
+++ precache-manifest.86a45d8fc69fbdcfe238a105390ab7bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90eec4722e43a73ef9528c67693c042e",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "eff2d4449b4c980e5aa1",
+    "url": "/my-app/static/css/main.e81a138b.chunk.css"
+  },
+  {
+    "revision": "aa6984cd07520a83fd66",
+    "url": "/my-app/static/js/2.0a57a6d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.0a57a6d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eff2d4449b4c980e5aa1",
+    "url": "/my-app/static/js/main.f25578fa.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
